ce1276ba83ada0675d0ae622387b8af812a05601
tiagomota <tiago.vieira.mota@gmail.com>
2022-07-25 15:48:41 -0300

    inclus input

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as VComponentsSample } from './v-components-sample.vue';
+export { default as VComponentInput } from './v-component-input.vue';
---
+++ src/lib-components/v-component-input.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input @change="onChange" v-model="valorCopy" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    valor: {
+      type: String,
+      default: '1000'
+    }
+  },
+  mounted() {
+    this.valorCopy = this.padraoMoedaBrasil.format(this.valorCopy);
+  },
+  data() {
+    return {
+      valorCopy: this.valor,
+      padraoMoedaBrasil: new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        minimumFractionDigits: 2
+      })
+    };
+  },
+  methods: {
+    onChange({ target }) {
+      this.valorCopy = this.padraoMoedaBrasil.format(target.value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
