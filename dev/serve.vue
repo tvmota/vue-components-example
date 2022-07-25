b59cd39b0fa7d997e20eeedfa7a140c7ddc753fa
tiagomota <tiago.vieira.mota@gmail.com>
2022-07-25 15:03:54 -0300

    commit inicial

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VComponentsSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <v-components-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VComponentsSample } from './v-components-sample.vue';
